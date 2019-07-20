1c488dd431daeaad42c0b74cf0294d9ac9f300b7
vagrant <vagrant@vagrant.vm>
2019-07-20 19:25:41 +0000

    initial files

---
+++ server.py
@@ -0,0 +1,28 @@
+from pprint import pformat
+import os
+import requests
+from flask import Flask, render_template, redirect, flash, session, request, jsonify
+import jinja2
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.secret_key = 'xb2@<4*axd3x7fxb9x9bxdcD&x88x01xe0xd2x9dxdcxcc4nxf3v'
+
+
+@app.route("/")
+def homepage():
+    """Show homepage"""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0")
+
+
+
