ce9e12e2f75c5d3c171f5ae646332ad480a98d7a
Gabriela Gerson <gabgerson1@gmail.com>
2019-07-20 20:34:59 -0700

    Can send user route end point to backend

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, flash, session, request, jso
 import jinja2
 from flask_debugtoolbar import DebugToolbarExtension
 
+
 app = Flask(__name__)
 
 app.secret_key = 'xb2@<4*axd3x7fxb9x9bxdcD&x88x01xe0xd2x9dxdcxcc4nxf3v'
@@ -14,6 +15,15 @@ def homepage():
     """Show homepage"""
 
     return render_template("homepage.html")
+
+@app.route("/charging", methods=["GET", "POST"])
+def charging():
+    """Show homepage"""
+    latitude = request.form.get("latitude")
+    longitude = request.form.get("longitude")
+    print((latitude,longitude))
+
+    return jsonify(latitude)
 
 
 if __name__ == "__main__":
