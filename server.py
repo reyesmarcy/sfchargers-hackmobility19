88eacec10c7a367d4bb1c0a7ba13e4805913f9c9
Gabriela Gerson <gabgerson1@gmail.com>
2019-07-20 22:09:05 -0700

    Can drop markers for charging stations at end of user route

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask, render_template, redirect, flash, session, request, jsonify
 import jinja2
 from flask_debugtoolbar import DebugToolbarExtension
-
+from nrel import *
 
 app = Flask(__name__)
 
@@ -19,12 +19,22 @@ def homepage():
 @app.route("/charging", methods=["GET", "POST"])
 def charging():
     """Show homepage"""
-    latitude = request.form.get("latitude")
-    longitude = request.form.get("longitude")
+
+    latitude = request.args.get("latitude")
+    longitude = request.args.get("longitude")
+    print(type(latitude))
     print((latitude,longitude))
-
-    return jsonify(latitude)
-
+    response = get_chargers_by_location(float(latitude),float(longitude))
+    print(response["fuel_stations"][0]["latitude"])
+    station_list = response["fuel_stations"]
+    station_dict = {}
+    for station in station_list:
+        station_latitude = station['latitude']
+        station_longitude = station['longitude']
+        station_dict[station_latitude] = station_longitude
+        print(station_latitude, station_longitude)
+    print(station_dict)
+    return jsonify(station_dict)
 
 if __name__ == "__main__":
     app.debug = True
